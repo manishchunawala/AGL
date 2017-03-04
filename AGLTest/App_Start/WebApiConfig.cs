749dcf3f7899839d6ecf21158828ffb6755cc99a
Manish Ashokbhai Chunawala (g) <manish.chunawala@avanade.com>
2017-03-04 20:49:07 +1100

    Dependency Injection and Unit Test commit

--- AGLTest/App_Start/WebApiConfig.cs
+++ AGLTest/App_Start/WebApiConfig.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using Autofac;
+using Autofac.Integration.WebApi;
+using AGLTest.DependencyInjection;
 
 namespace AGLTest
 {
@@ -12,6 +15,9 @@ namespace AGLTest
     {
         public static void Register(HttpConfiguration config)
         {
+            // Web API configuration and services
+            config.RegisterAutofac();
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
@@ -25,6 +31,18 @@ namespace AGLTest
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        public static void RegisterAutofac(this HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterTriggerModules();
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+        public static void RegisterTriggerModules(this ContainerBuilder builder)
+        {
+            builder.RegisterModule<DependencyModule>();
         }
     }
 }
---
+++ AGLTest/DependencyInjection/DependencyModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Module = Autofac.Module;
+
+namespace AGLTest.DependencyInjection
+{
+    public class DependencyModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+        }
+    }
+}
